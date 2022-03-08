d3bba7ac1d2d26d6a99aa8cfcd95da971af106f4
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2022-03-08 21:22:27 +1100

    Create dvdrental7.sql

---
+++ dvdrental7.sql
@@ -0,0 +1,15 @@
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating
+
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50
+ORDER BY COUNT(title) ASC;
+
+SELECT store_id, COUNT(customer_id) FROM customer
+GROUP BY store_id
+
+SELECT country_id, COUNT(city_id) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city_id) DESC
+LIMIT 1

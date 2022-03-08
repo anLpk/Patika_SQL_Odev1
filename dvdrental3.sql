8b3bbda61caec9d19460e44e6326e97d923b146c
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2022-03-08 16:50:34 +1100

    Update dvdrental3.sql

--- dvdrental3.sql
+++ dvdrental3.sql
@@ -1,1 +1,13 @@
+SELECT * FROM Country
+WHERE country LIKE 'A%a';
 
+SELECT * FROM Country
+WHERE country LIKE '_____%n';
+
+SELECT title, * FROM film
+WHERE title ILIKE '%T%T%T%T%'
+
+SELECT title, length, rental_rate, * FROM film
+WHERE title ILIKE 'C%'
+AND length > 90
+AND rental_rate = 2.99
